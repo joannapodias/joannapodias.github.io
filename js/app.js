1d560265e1339e22ca352b540db2ea627417d3d2
Joanna Podias <joannapodias@Joannas-Air.fios-router.home>
2019-07-28 15:09:00 -0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var wowCallback = function(box){
+}
+
+var titleSwitcher = function(){
+ $('.title').on('click', function(){
+   var color = $(this).data("bgcolor")
+   $('body').css({'background-color': color});
+   
+   if($(this).hasClass('joanna')){
+     $('.menu-container .title').removeClass('selected')     
+     $('.menu-container .title').removeClass('outline');
+     $('.footer a').removeClass('white');     
+   }else{
+     $('.menu-container .title').removeClass('selected')
+     $('.menu-container .title').addClass('outline')   
+     $(this).addClass('selected').removeClass('outline');
+     $('.joanna').removeClass('selected')        
+     $('.footer a').addClass('white');
+   }
+
+ })
+}
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    new WOW({
+    offset:100,
+    callback: function(t) {
+      wowCallback(t);
+    }
+    }).init()
+    
+    
+    // initializers
+    titleSwitcher();
+});
+
+
+
