2643894585686c8a6abbaf048bfab16f904fc86a
joannapodias <42387976+joannapodias@users.noreply.github.com>
2019-08-05 14:50:12 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,13 +5,7 @@ var titleSwitcher = function(){
  $('.title').on('click', function(){
    var color = $(this).data("bgcolor")
    $('body').css({'background-color': color});
-  $('blobs').css({'-webkit-transition': 'opacity 0.5s linear'});
-   $('blobs').css({'-moz-transition': 'opacity 0.5s linear'});
-   $('blobs').css({'-ms-transition': 'opacity 0.5s linear'});
-   $('blobs').css({'-o-transition': 'opacity 0.5s linear'});
-   $('blobs').css({'transition': 'opacity 0.5s linear'});
-  
-   
+ 
    
    if($(this).hasClass('joanna')){
      $('.menu-container .title').removeClass('selected')     
